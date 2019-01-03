d0f44017a6aaed38066be504fdded1f775fd257d
Andre Silva <andreqls@gmail.com>
2019-01-03 21:35:05 -0200

    Directed "spend-memory" strategy according to top comment

--- PRIME1/prime1.cpp
+++ PRIME1/prime1.cpp
@@ -1,13 +1,10 @@
-// A simple "Sieve of Erathostenes" implementation of PRIME1 from SPOJ.
-// The next versions will focus on performance. Two ideas:
-// 1. primelist[n]=value which determines immediately if n is prime, composite or untested.
-// 	NOTE: Untested value might be replaced by a global maximum.
-// 2. Maintain an ordered list (AVL tree?) of previously determined primes.
+// primelist[n]=value which determines immediately if n is prime, composite or untested.
+// NOTE: Untested value might be replaced by a global maximum.
 
 #include <iostream>
 
 int T,N;
-int primelist[10][100000];
+int primelist[1000000];
 
 bool isPrime(int candidate) {
     int i;
